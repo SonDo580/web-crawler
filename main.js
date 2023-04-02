3d63ff582c4811ffc79f5af21c0ba96558507152
Son Do <dominhson580@gmail.com>
2023-04-02 21:46:30 +0700

    Define main()

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+const { crawlPage } = require("./crawl");
+
+async function main() {
+  if (process.argv.length !== 3) {
+    console.log("Usage: npm start <baseURL>");
+    process.exit(1);
+  }
+
+  const baseURL = process.argv[2];
+  console.log(`Start crawling ${baseURL}`);
+
+  const pages = await crawlPage(baseURL, baseURL, {});
+  console.log(pages);
+}
+
+main();
