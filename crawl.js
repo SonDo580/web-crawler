2e93176bdbeced7b6a1ea0fb623a2dfd2c4ed7a2
Son Do <dominhson580@gmail.com>
2023-04-02 15:56:01 +0700

    Define normalizeURL()

--- crawl.js
+++ crawl.js
@@ -1,5 +1,11 @@
 function normalizeURL(url) {
-  return url;
+  const urlObj = new URL(url);
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
 }
 
 module.exports = {
