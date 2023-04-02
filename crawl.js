cd459af76349f002f5e0717e4f5699ee10d4ba4e
Son Do <dominhson580@gmail.com>
2023-04-02 17:02:24 +0700

    Define getURLsFromHTML()

--- crawl.js
+++ crawl.js
@@ -1,3 +1,6 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
 function normalizeURL(url) {
   const urlObj = new URL(url);
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -8,10 +11,32 @@ function normalizeURL(url) {
   return hostPath;
 }
 
-function getURLsFromHTML(htmlBody, baseURL) {
-  return [];
+function getURLsFromHTML(html, baseURL) {
+  const dom = new JSDOM(html);
+  const linkElems = dom.window.document.querySelectorAll("a");
+
+  const urls = [];
+  for (const linkElem of linkElems) {
+    const linkHref = linkElem.href;
+    try {
+      if (linkHref.slice(0, 1) === "/") {
+        // relative path
+        const urlObj = new URL(`${baseURL}${linkHref}`);
+        urls.push(urlObj.href);
+      } else {
+        // absolute path
+        const urlObj = new URL(`${linkHref}`);
+        urls.push(urlObj.href);
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  return urls;
 }
 
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
